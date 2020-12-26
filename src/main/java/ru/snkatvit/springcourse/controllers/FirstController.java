0fc0a30e8f23fd08b55a28b8f7967e67b7919317
Spartak Katvickii <bonus2k@gmail.com>
2020-12-27 00:06:38 +0400

    lesson 19 @RequestParam

--- src/main/java/ru/snkatvit/springcourse/controllers/FirstController.java
+++ src/main/java/ru/snkatvit/springcourse/controllers/FirstController.java
@@ -3,13 +3,21 @@ package ru.snkatvit.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
